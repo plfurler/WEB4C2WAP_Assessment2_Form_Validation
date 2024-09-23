f5de114643ece4e53d599e07769123727876e9ca
plfurler <plfurler@gmail.com>
2024-09-23 16:40:49 +0930

    Added the html and js code to dynamic populate the states for both Australia and NZ

--- js/validate.js
+++ js/validate.js
@@ -33,3 +33,47 @@ function validateErrors(formField, errorField) {
     return true;
   }
 }
+
+function changeState() {
+  // store a reference to country and state select lists
+  var country = document.getElementById("country").value;
+  var state = document.getElementById("state");
+
+  // if country selected is Australia
+  // populate state select list with Australian states
+  if (country === "AU") {
+    state.innerHTML = "";
+
+    state.options[state.options.length] = new Option("-- select state --", "0");
+    state.options[state.options.length] = new Option("South Australia", "SA");
+    state.options[state.options.length] = new Option("New South Wales", "NSW");
+    state.options[state.options.length] = new Option("Victoria", "VIC");
+    state.options[state.options.length] = new Option("Queensland", "QLD");
+    state.options[state.options.length] = new Option("Tasmania", "TAS");
+    state.options[state.options.length] = new Option("Western Australia", "WA");
+    state.options[state.options.length] = new Option(
+      "Australian Capital Territory",
+      "ACT"
+    );
+    state.options[state.options.length] = new Option(
+      "Northern Territory",
+      "NT"
+    );
+  }
+
+  // if country selected is New Zealand
+  // populate state select list with New Zealand regions
+  if (country === "NZ") {
+    state.innerHTML = ""; // Clear existing options
+
+    state.options[state.options.length] = new Option(
+      "-- select region --",
+      "0"
+    );
+    state.options[state.options.length] = new Option("Auckland", "AUK");
+    state.options[state.options.length] = new Option("Wellington", "WGN");
+    state.options[state.options.length] = new Option("Canterbury", "CAN");
+    state.options[state.options.length] = new Option("Otago", "OTA");
+    // Add more regions as needed
+  }
+}
