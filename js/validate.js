a8065fa93debcd269b116ef68b8013af82edc8c5
plfurler <plfurler@gmail.com>
2024-09-25 00:28:28 +0930

    Added JS validatePostcode(), and onblur event attribute to input id postcode

--- js/validate.js
+++ js/validate.js
@@ -43,7 +43,6 @@ function changeState() {
   // populate state select list with Australian states
   if (country === "AU") {
     state.innerHTML = "";
-
     state.options[state.options.length] = new Option("-- select state --", "0");
     state.options[state.options.length] = new Option("South Australia", "SA");
     state.options[state.options.length] = new Option("New South Wales", "NSW");
@@ -60,20 +59,113 @@ function changeState() {
       "NT"
     );
   }
-
   // if country selected is New Zealand
   // populate state select list with New Zealand regions
-  if (country === "NZ") {
-    state.innerHTML = ""; // Clear existing options
-
+  else if (country === "NZ") {
+    state.innerHTML = "";
     state.options[state.options.length] = new Option(
       "-- select region --",
       "0"
     );
-    state.options[state.options.length] = new Option("Auckland", "AUK");
-    state.options[state.options.length] = new Option("Wellington", "WGN");
-    state.options[state.options.length] = new Option("Canterbury", "CAN");
-    state.options[state.options.length] = new Option("Otago", "OTA");
-    // Add more regions as needed
+    state.options[state.options.length] = new Option("Auckland", "AU");
+    state.options[state.options.length] = new Option("Canterbury", "CA");
+    state.options[state.options.length] = new Option("Wellington", "WE");
+    state.options[state.options.length] = new Option("Waikato", "WK");
+    state.options[state.options.length] = new Option("Bay of Plenty", "BP");
+    state.options[state.options.length] = new Option(
+      "Manawatū-Whanganui",
+      "MW"
+    );
+    state.options[state.options.length] = new Option("Otago", "OT");
+    state.options[state.options.length] = new Option("Northland", "NL");
+    state.options[state.options.length] = new Option("Hawke's Bay", "HB");
+    state.options[state.options.length] = new Option("Taranaki", "TK");
+    state.options[state.options.length] = new Option("Southland", "SL");
+  }
+  // if no country selected populate state select list
+  // with one option that tells user to select country first
+  else {
+    state.innerHTML = "";
+    state.options[state.options.length] = new Option(
+      "-- select country first --",
+      ""
+    );
+  }
+
+  // Change background color of country select element
+  if (document.getElementById("country").value == "0") {
+    document.getElementById("country").style.background = "#FF9999";
+  } else {
+    document.getElementById("country").style.background = "#CCFFCC";
+  }
+}
+
+// Call this function using an onchange event on your state select
+// Add the same if statement into the end of function changeState()
+// and change the id from your state select to your country select
+function stateColours() {
+  if (document.getElementById("state").value == "0") {
+    document.getElementById("state").style.background = "#FF9999";
+  } else {
+    document.getElementById("state").style.background = "#CCFFCC";
+  }
+}
+
+function validatePostcode() {
+  var state = document.getElementById("state").value;
+  var postcode = document.getElementById("postcode").value;
+  var postcodeError = document.getElementById("postcodeError");
+  var postcodeInput = document.getElementById("postcode");
+  var postcodeRegExp;
+  var errorMessage = "";
+
+  switch (state) {
+    case "NSW":
+    case "ACT":
+      postcodeRegExp = /^[2][0-9]{3}$/;
+      errorMessage = "Postcode should start with 2 for NSW and ACT.";
+      break;
+    case "VIC":
+      postcodeRegExp = /^[3][0-9]{3}$/;
+      errorMessage = "Postcode should start with 3 or 4 for VIC.";
+      break;
+    case "QLD":
+      postcodeRegExp = /^[4][0-9]{3}$/;
+      errorMessage = "Postcode should start with 4 to 9 for QLD.";
+      break;
+    case "SA":
+      postcodeRegExp = /^[5][0-9]{3}$/;
+      errorMessage = "Postcode should start with 5 for SA.";
+      break;
+    case "WA":
+      postcodeRegExp = /^[6][0-9]{3}$/;
+      errorMessage = "Postcode should start with 6 for WA.";
+      break;
+    case "TAS":
+      postcodeRegExp = /^[7][0-9]{3}$/;
+      errorMessage = "Postcode should start with 7 for TAS.";
+      break;
+    case "NT":
+      postcodeRegExp = /^[0][0-9]{3}$/;
+      errorMessage = "Postcode should start with 0 for NT.";
+      break;
+    default:
+      postcodeRegExp = /^[0-9]{4}$/;
+      errorMessage = "Please enter a valid postcode.";
+  }
+
+  if (!postcodeRegExp.test(postcode)) {
+    postcodeError.style.display = "block";
+    postcodeError.textContent = postcodeInput.title;
+    postcodeError.textContent += errorMessage;
+    // Set field background to red
+    postcodeInput.style.backgroundColor = "#FF9999";
+    postcodeInput.focus();
+    return false;
+  } else {
+    postcodeError.style.display = "none";
+    // Set field background to green
+    postcodeInput.style.backgroundColor = "#CCFFCC"; 
+    return true;
   }
 }
